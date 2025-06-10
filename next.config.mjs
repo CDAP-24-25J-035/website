0ccb507fb0ef5995f94e9e6ffb015367564d0d01
saputhebeast <sapumalwijekoon4@gmail.com>
2025-06-11 01:29:47 +0530

    Feat: deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   basePath: '/website',
   assetPrefix: '/website/',
-}
-
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
 export default nextConfig
