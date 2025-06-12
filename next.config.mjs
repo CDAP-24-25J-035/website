e1e8935de610787f94bc0b0f497072bb865d6f2d
saputhebeast <sapumalwijekoon4@gmail.com>
2025-06-12 20:48:02 +0530

    Feat: new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
-// next.config.mjs
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: isProd ? '/website' : '',
-  assetPrefix: isProd ? '/website/' : '',
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  /* config options here */
 };
 
 export default nextConfig;
